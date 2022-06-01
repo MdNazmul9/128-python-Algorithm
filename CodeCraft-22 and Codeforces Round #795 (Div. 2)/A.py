338ef10d3d235403177e7cc5479adb9bd85b683d
MdNazmul9 <nazmul.cse48@gmail.com>
2022-06-01 22:22:35 +0600

    795 div2 A solved

--- CodeCraft-22 and Codeforces Round #795 (Div. 2)/A.py
+++ CodeCraft-22 and Codeforces Round #795 (Div. 2)/A.py
@@ -1,18 +1,18 @@
-# list = [2, 4, 3, 6, 8]
-# list = [3, 5, 9, 7, 1, 3]
-
-i = 0
-dupe = False
-cnt = 0
-while i < len(list)-1:
-    if (list[i] + list[i+1]) %2 != 0:
-        del list[i+1]
-        cnt+=1
-        dupe = True
-    elif dupe:
-        del list[i]
-        dupe = False
-    else:
-        i += 1
+from sys import stdin
+for i in range(int(stdin.readline())):
+    k = int(stdin.readline())
+    arr = list(map(int, stdin.readline().split()))
+    cnt = [0, 0]
+    for a in arr:
+        cnt [a & 1] +=1
+    print(min(cnt))
+    
+    
         
-print(cnt)
+    
+            
+
+        
+            
+
+        
